b1f2c0891e850b74a8786eabfbb0531819311d1a
Zari_Tsu <zaritsu030907@gmail.com>
2024-08-01 20:33:02 +0800

    add interceptor for login status

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import lombok.val;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 获取session中的用户信息
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("use");
+        // 如果用户不存在就拦截，发送401错误码
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+        // 如果用户存在则保存在本地，表示已登录状态
+        UserHolder.saveUser((User) user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+
+}
